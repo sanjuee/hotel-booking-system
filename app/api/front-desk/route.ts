304802d0a918995f9a2205b8fcef70950d0ae4af
sanju <sanjaysathish168@gmail.com>
2026-04-15 15:11:20 +0530

    feat: add GET and PATCH endpoints for managing room unit status in front desk API

---
+++ app/api/front-desk/route.ts
@@ -0,0 +1,46 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+import { unstable_isUnrecognizedActionError } from "next/dist/client/components/navigation.react-server";
+
+export async function GET() {
+    try {
+        const allUnits = await prisma.roomUnit.findMany({
+            include: {
+                room:{
+                    select: { name: true, type: true}
+                }
+            },
+            orderBy: { roomNumber: 'asc'}
+        })
+
+        return NextResponse.json(allUnits, { status: 200 })
+
+    } catch (error) {
+        return NextResponse.json(
+            { error: "Failed to fetch front desk data" }, 
+            { status: 500 }
+        )
+    }
+}
+
+export async function PATCH(request: Request){
+    try {
+        const body = await request.json()
+        const { unitId, newStatus } = body
+
+        if (!unitId || !newStatus) {
+            return NextResponse.json({ error: "Missing data" }, { status: 400 });
+        }
+
+        const updatedUnit = await prisma.roomUnit.update({
+            where: { id: unitId },
+            data:{ status: newStatus }
+        })
+
+        return NextResponse.json(updatedUnit, { status: 200})
+
+    } catch (error) {
+        console.error("Status update failed:", error);
+        return NextResponse.json({ error: "Failed to update status" }, { status: 500 })
+    }
+}
